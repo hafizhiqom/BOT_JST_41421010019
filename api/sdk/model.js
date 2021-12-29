7a2f77c01fc017135fdd4c7eafff53d523eb4a21
hafizhiqom <95557683+hafizhiqom@users.noreply.github.com>
2021-12-30 03:40:48 +0700

    Update model.js

--- api/sdk/model.js
+++ api/sdk/model.js
@@ -23,7 +23,7 @@ async function predict(data){
 
     try{
         // path load in public access => github
-        const path = 'https://raw.githubusercontent.com/zendi014/jst_service/main/public/ex_model/model.json';
+        const path = 'https://raw.githubusercontent.com/hafizhiqom/BOT_JST_41421010019/main/public/ex_model/model.json';
         const model = await tf.loadGraphModel(path);
         
         predict = model.predict(
